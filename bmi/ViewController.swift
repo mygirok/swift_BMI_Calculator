0225b84aaea982b916d6aca42b3cdbc9c50f4463
mygirok <forgirok@gmail.com>
2021-11-08 21:03:36 +0900

    rouonded, user defined runtime attr

--- bmi/ViewController.swift
+++ bmi/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtHeight: UITextField!
+    @IBOutlet var txtWeight: UITextField!
+    @IBOutlet var lblResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func clacBmi(_ sender: UIButton) {
+    }
+    
 
 }
 
