46642d3d4538e431b7024d2f6d636bde6eea89a3
mygirok <forgirok@gmail.com>
2021-11-09 22:15:34 +0900

    get text from textfield

--- bmi/ViewController.swift
+++ bmi/ViewController.swift
@@ -17,6 +17,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func clacBmi(_ sender: UIButton) {
+        let height = Double(txtHeight.text!)!
+        let weight = Double(txtWeight.text!)!
+        print(height, weight)
+        
+        let bmi = weight / (height * height * 0.0001)
+        let shortenedBmi = String(format: "%.1f", bmi)
+        var body  = ""
+        if bmi >= 40 {
+            body = "Class III"
+        } else if bmi >= 30 && bmi < 40 {
+            body = "Class II"
+        } else if bmi >= 25 && bmi < 30 {
+            body = "Class I"
+        } else if bmi >= 18.5 && bmi < 25 {
+            body = "Normal range"
+        } else {
+            body = "Underweight"
+        }
+        
+        print("BMI: \(shortenedBmi), Category: \(body)")
     }
     
 
