159099a20dc9108d260364a8509f295e8b7f27ff
mygirok <forgirok@gmail.com>
2021-11-10 22:28:10 +0900

    check empty input, add color

--- bmi/ViewController.swift
+++ bmi/ViewController.swift
@@ -17,26 +17,43 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func clacBmi(_ sender: UIButton) {
-        let height = Double(txtHeight.text!)!
-        let weight = Double(txtWeight.text!)!
-        print(height, weight)
-        
-        let bmi = weight / (height * height * 0.0001)
-        let shortenedBmi = String(format: "%.1f", bmi)
-        var body  = ""
-        if bmi >= 40 {
-            body = "Class III"
-        } else if bmi >= 30 && bmi < 40 {
-            body = "Class II"
-        } else if bmi >= 25 && bmi < 30 {
-            body = "Class I"
-        } else if bmi >= 18.5 && bmi < 25 {
-            body = "Normal range"
+        if txtHeight.text == "" || txtWeight.text == "" {
+//            print("Input error")
+            lblResult.text = "Please put Height and Weight"
+            return
         } else {
-            body = "Underweight"
+            let height = Double(txtHeight.text!)!
+            let weight = Double(txtWeight.text!)!
+//            print(height, weight)
+            
+            let bmi = weight / (height * height * 0.0001)
+            let shortenedBmi = String(format: "%.1f", bmi)
+            var body  = ""
+            var color = UIColor.white
+            
+            if bmi >= 40 {
+                color = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+                body = "Class III"
+            } else if bmi >= 30 && bmi < 40 {
+                color = UIColor(displayP3Red: 0.7, green: 0.0, blue: 0.0, alpha: 1.0)
+                body = "Class II"
+            } else if bmi >= 25 && bmi < 30 {
+                color = UIColor(displayP3Red: 0.4, green: 0.0, blue: 0.0, alpha: 1.0)
+                body = "Class I"
+            } else if bmi >= 18.5 && bmi < 25 {
+                color = UIColor(displayP3Red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+                body = "Normal range"
+            } else {
+                color = UIColor(displayP3Red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
+                body = "Underweight"
+            }
+            
+//            print("BMI: \(shortenedBmi), Category: \(body)")
+            lblResult.backgroundColor = color
+            lblResult.clipsToBounds = true
+            lblResult.layer.cornerRadius = 20
+            lblResult.text = "BMI: \(shortenedBmi), Category: \(body)"
         }
-        
-        print("BMI: \(shortenedBmi), Category: \(body)")
     }
     
 
